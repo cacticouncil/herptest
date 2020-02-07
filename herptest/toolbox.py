74d127a341df3718ee5bea97fcec83209f05b6a8
Jeremiah Blanchard <jeremiahblanchard@gmail.com>
2020-02-07 17:04:23 -0500

    Adjusted function names to match common python style; added PipeSet class

--- herptest/toolbox.py
+++ herptest/toolbox.py
@@ -4,13 +4,60 @@ import hashlib
 import tempfile
 import shutil
 import os
+import sys
 
 import importlib.util as import_util
 from ctypes import util
 from os import path
 
+DEFAULT_MAX_READ = 1024 * 1024
 
-def loadModule(filename, module_name=None):
+class PipeSet:
+    """Class wrapping python pipes as a set to make it easier to read / write them"""
+    def __init__(self):
+        self.pipe_in, self.pipe_out = os.pipe()
+
+
+    def write(self, bytes):
+        os.write(self.pipe_out, bytes)
+
+
+    def read(self, maxbytes = DEFAULT_MAX_READ):
+        return os.read(self.pipe_in, maxbytes)
+
+
+    def print(self, *argv, **kwargs):
+        message = ''
+        for index, arg in enumerate(argv):
+            if not isinstance(arg, str):
+                arg = str(arg)
+
+            # Add separators before argument elements except for the first element
+            message = message + ((kwargs['sep'] if 'sep' in kwargs else ' ') if index > 0 else '') + arg
+
+        message = message + (kwargs['end'] if 'end' in kwargs else '')
+        self.write(message.encode('utf-8'))
+
+
+    def println(self, *argv, **kwargs):
+        kwargs['end'] = '\n'
+        self.print(*argv, **kwargs)
+
+
+def data_to_file(data, filename):
+    with open(filename, 'w+') as my_file:
+        my_file.write(data)
+        my_file.close()
+
+
+def write_to_file(lines, filename):
+    with open(filename, 'w+') as my_file:
+        for line in lines:
+            my_file.write(line + "\n")
+        my_file.close()
+
+
+def load_module(filename, module_name=None):
     if not module_name:
         module_name = 'unnamed_module.' + path.basename(path.splitext(filename)[0])
 
@@ -26,11 +73,35 @@ def loadModule(filename, module_name=None):
     return module
 
 
+def is_file(filename):
+    if path.isfile(filename):
+        return filename
+    else:
+        return None
+
+
+def find_library(directory, name):
+    result = (util.find_library(path.join(directory, name)) or
+              util.find_library(path.join(directory, name, name)) or
+              util.find_library(path.join(directory, "lib" + name)) or
+              util.find_library(path.join(directory, name, "lib" + name)) or
+              util.find_library(path.join(directory, "lib" + name + '.so')) or
+              util.find_library(path.join(directory, name, "lib" + name + '.so')))
+
+    return (result or
+        is_file(path.join(directory, name)) or
+        is_file(path.join(directory, name, name)) or
+        is_file(path.join(directory, "lib" + name)) or
+        is_file(path.join(directory, name, "lib" + name)) or
+        is_file(path.join(directory, "lib" + name + '.so')) or
+        is_file(path.join(directory, name, "lib" + name + '.so')))
+
+
 # Hack: To get Python to load a DLL temporarily - so that it can be replaced later - we need to load a different name.
 # To do this, we'll make a copy of the library, load it, then dispense with it when we're done.
 def loadTempLibrary(directory, name):
     #print("Loading", name, "from", directory)
-    libPath = findLibrary(directory, name) or path.join(directory, name, "lib" + name + '.so')
+    libPath = find_library(directory, name) or path.join(directory, name, "lib" + name + '.so')
     hexHash = hashlib.md5(open(libPath,'rb').read()).hexdigest()
     #print("Loading", name, "from", directory, ":", libPath)
 
@@ -40,15 +111,9 @@ def loadTempLibrary(directory, name):
     #print("Loaded as", libTemp)
     return ctypes.cdll.LoadLibrary(libTemp)
 
-def isFile(filename):
-    if path.isfile(filename):
-        return filename
-    else:
-        return None
-
 
 def loadLibrary(directory, name):
-    libPath = findLibrary(directory, name) or path.join(directory, "lib" + name + '.so')
+    libPath = find_library(directory, name) or path.join(directory, "lib" + name + '.so')
     return ctypes.CDLL(libPath, mode=ctypes.RTLD_GLOBAL)
 
 
@@ -57,20 +122,3 @@ def unloadLibrary(library):
         _ctypes.FreeLibrary(library)
     else:
         _ctypes.dlclose(library._handle)
-
-
-def findLibrary(directory, name):
-    result = (util.find_library(path.join(directory, name)) or
-              util.find_library(path.join(directory, name, name)) or
-              util.find_library(path.join(directory, "lib" + name)) or
-              util.find_library(path.join(directory, name, "lib" + name)) or
-              util.find_library(path.join(directory, "lib" + name + '.so')) or
-              util.find_library(path.join(directory, name, "lib" + name + '.so')))
-
-    return (result or
-        isFile(path.join(directory, name)) or
-        isFile(path.join(directory, name, name)) or
-        isFile(path.join(directory, "lib" + name)) or
-        isFile(path.join(directory, name, "lib" + name)) or
-        isFile(path.join(directory, "lib" + name + '.so')) or
-        isFile(path.join(directory, name, "lib" + name + '.so')))

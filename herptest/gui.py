b5693d5ea249a586970e4fcda6a43e2ab30ed000
Jack <harolds_m@yahoo.com>
2023-07-13 12:59:27 +0900

    fix - Update Headers

--- herptest/gui.py
+++ herptest/gui.py
@@ -1,32 +1,32 @@
 import sys, os
 import time, random, pathlib
 from PySide2 import QtCore, QtWidgets, QtGui
-import homePage, testSuiteCreator, resultsPage, canvasUploadPage, autopullElmaPage, vmPage
+from herptest import homePage, testSuiteCreator, resultsPage, canvasUploadPage, autopullElmaPage, vmPage
 
 
 
 def initEnviron():
     os.environ["LIBGL_ALWAYS_INDIRECT"] = "1" #necessary to avoid openGL errors with accessing Windows openGL from WSL
-    os.environ["XDG_RUNTIME_DIR"] = "/tmp/peng-runtime" #used by qt for cache
+    os.environ["XDG_RUNTIME_DIR"] = "/tmp/herp-runtime" #used by qt for cache
 def initWindow():
 
     window = QtWidgets.QMainWindow()
-
+    
     tabContainer =  QtWidgets.QTabWidget()
 
     #create all the tabs for the gui, save a reference to them, and add them to the tab creator
     tabContainer.addTab(homePage.HomePage(), "Run HerpTest")
     homePageInst = tabContainer.widget(0)
-    # tabContainer.addTab(testSuiteCreator.TestSuiteCreator(), "Create Test Suite")
-    # testSuiteCreatorInst = tabContainer.widget(1)
+    tabContainer.addTab(testSuiteCreator.TestSuiteCreator(), "Create Test Suite")
+    testSuiteCreatorInst = tabContainer.widget(1)
     tabContainer.addTab(resultsPage.ResultsPage(), "Test Results")
-    resultsPageInst = tabContainer.widget(1)
+    resultsPageInst = tabContainer.widget(2)
     tabContainer.addTab(canvasUploadPage.CanvasUploadPage(), "Canvas Uploader")
-    canvasUploaderInst = tabContainer.widget(2)
+    canvasUploaderInst = tabContainer.widget(3)
     tabContainer.addTab(autopullElmaPage.AutopullElmaPage(), "Auto-Pull && ELMA")
-    elmaInst = tabContainer.widget(3)
+    elmaInst = tabContainer.widget(4)
     tabContainer.addTab(vmPage.VmPage(), "VM Config")
-    vmPageInst = tabContainer.widget(4)
+    vmPageInst = tabContainer.widget(5)
 
 
     #give the home page the funcion to call when the SHOW RESULTS button is clicked
@@ -42,7 +42,7 @@ def initWindow():
     window.setCentralWidget(tabContainer)
     window.setWindowTitle("HerpTest")
     window.resize(800, 800)
-
+    
     createStatusBar(window)
     return window
 
@@ -67,7 +67,7 @@ def main():
     initEnviron()
     app = QtWidgets.QApplication([])
 
-    #handle the no-splash option
+    #handle the no-splash option 
     if len(sys.argv) > 1 and sys.argv[1] == "--no-splash":
         window = initWindow()
         window.show()
